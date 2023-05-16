2068be9a8ebfcb9d1dae953bd1faac5fe304d9a0
RafaelMALima <rafaelmal@al.insper.edu.br>
2023-05-16 12:33:26 -0300

    iqa mais fofo

--- IQA.py
+++ IQA.py
@@ -1,8 +1,5 @@
-def IQA(qi,wi):
-    iqa=1 # Índice de Qualidade das Águas, um número entre 0 e 100;
-    # qi [i]: qualidade do i-ésimo parâmetro, um número entre 0 e 100, obtido da respectiva “curva média devariação de qualidade”, em função de sua concentração ou medida
-    # wi [i]: peso correspondente ao i-ésimo parâmetro, um número entre 0 e 1, atribuído em função da sua importância para a conformação global de qualidade
-    for i in range(0,i):
-        iqa*=qi[i]**wi[i]
-        
-    return iqa
+w_i = [0,0.15,0.12,0.10,0.10,0.10,0.10,0.08,0.08,0.17]
+iqa_parcial =lambda q,i : q**w_i[i]
+iqa = 1
+q_i = []
+for i in range(len(w_i)): iqa*= iqa_parcial(q_i[i], i)
