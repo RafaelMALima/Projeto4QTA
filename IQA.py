547c7dcc2379929d51e8aa8df04fbe90820784f0
RafaelMALima <rafaelmal@al.insper.edu.br>
2023-05-16 12:56:00 -0300

    fofo

--- IQA.py
+++ IQA.py
@@ -2,6 +2,5 @@ def IQA(q_i):
     w_i = [0,0.15,0.12,0.10,0.10,0.10,0.10,0.08,0.08,0.17]
     iqa_parcial =lambda q,i : q**w_i[i]
     iqa = 1
-    q_i = []
-    for i in range(1,len(w_i)): iqa*= iqa_parcial(q_i[i], i)
+    for i in range(1,len(w_i)): iqa*= iqa_parcial(q_i[i-1], i)
     return iqa
